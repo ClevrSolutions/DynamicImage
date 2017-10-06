aa231a3094c6f72cf35033a62e8fd4ac8bdd9d82
McDoyen <mrpoloh@yahoo.co.uk>
2017-10-06 12:28:05 +0300

    Remove fix deprecated dojo functions

--- src/DynamicImage/widget/MobileStaticImage.js
+++ src/DynamicImage/widget/MobileStaticImage.js
@@ -1,38 +1,38 @@
 define([
-    'dojo/_base/declare',
-    'mxui/widget/_WidgetBase',
-    "dijit/_TemplatedMixin",
-    "mxui/dom",
-    "dojo/_base/lang",
-    "dojo/text!DynamicImage/widget/templates/DynamicImage.html"
+	'dojo/_base/declare',
+	'mxui/widget/_WidgetBase',
+	"dijit/_TemplatedMixin",
+	"mxui/dom",
+	"dojo/_base/lang",
+	"dojo/text!DynamicImage/widget/templates/DynamicImage.html"
 ], function (declare, _WidgetBase, _TemplatedMixin, mxuiDom, lang, widgetTemplate) {
-    'use strict';
+	'use strict';
 
-    return declare('DynamicImage.widget.MobileStaticImage', [_WidgetBase, _TemplatedMixin], {
+	return declare('DynamicImage.widget.MobileStaticImage', [ _WidgetBase, _TemplatedMixin ], {
 
-        templateString: widgetTemplate,
+		templateString: widgetTemplate,
 
-        imageurl : '',
-	defaultImage : '',
-	alt : '',
-	_hasStarted : false,
+		imageurl: '',
+		defaultImage: '',
+		alt: '',
+		_hasStarted: false,
 
-	startup : function(){
-		if (this._hasStarted)
-			return;
+		startup: function () {
+			if (this._hasStarted)
+				return;
 
-		this._hasStarted = true;
+			this._hasStarted = true;
 
-		this.domNode.appendChild(
-            mxuiDom.create("img", {
-				src : (this.imageurl != '')?this.imageurl:this.defaultImage,
-				alt : this.alt
-			})
-		);
-		
-		this.actLoaded(function (){}); // temp. fix for a client issue, should be fixed in 4.4
-	}
+			this.domNode.appendChild(
+				mxuiDom.create("img", {
+					src: (this.imageurl != '') ? this.imageurl : this.defaultImage,
+					alt: this.alt
+				})
+			);
+
+			this.actLoaded(function () {}); // temp. fix for a client issue, should be fixed in 4.4
+		}
+	});
 });
-});
 
-require(['DynamicImage/widget/MobileStaticImage']);
+require([ 'DynamicImage/widget/MobileStaticImage' ]);
--- src/DynamicImage/widget/StaticImage.js
+++ src/DynamicImage/widget/StaticImage.js
@@ -7,7 +7,7 @@ define([
 ], function (declare, _WidgetBase, _TemplatedMixin, lang, widgetTemplate) {
   'use strict';
 
-  return declare('DynamicImage.widget.StaticImage', [_WidgetBase, _TemplatedMixin], {
+  return declare('DynamicImage.widget.StaticImage', [ _WidgetBase, _TemplatedMixin ], {
 
     templateString: widgetTemplate,
 
@@ -21,9 +21,8 @@ define([
         this.imageNode.src = this.imageurl;
       else
         this.imageNode.src = this.defaultImage;
-      this.actRendered();
     }
   });
 });
 
-require(['DynamicImage/widget/StaticImage']);
+require([ 'DynamicImage/widget/StaticImage' ]);
